ae8f387d97ddbc606d9148b83fde4853245123a9
Aleksander Smaga <165085833+asmagaa@users.noreply.github.com>
2025-05-31 13:49:47 +0200

    fix
    item is shown, everything works fine

---
+++ src/main/java/name/modid/item/ModItems.java
@@ -0,0 +1,26 @@
+package name.modid.item;
+
+import name.modid.Disenchanter;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static final Item DISENCHANTER = registerItem("disenchanter", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(Disenchanter.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        Disenchanter.LOGGER.info("Registering Mod Items for" + Disenchanter.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(DISENCHANTER);
+        });
+    }
+}
